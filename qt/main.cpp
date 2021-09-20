3d32df0593278491e6a99be903f1316c5bd73092
Joserivera-tech <jose.riverav@udea.edu.co>
2021-09-19 23:24:41 -0500

    Se agrego un ciclo para lee todos los pixeles e imprimir el valor RGB de cada uno

--- qt/main.cpp
+++ qt/main.cpp
@@ -5,9 +5,17 @@ using namespace std;
 int main()
 {
     Imagen *I;
+    int h,w;
     I = new Imagen(":/new/prefix1/Mov lados.png");
-
-    cout << I->GetColor(3,0, 'G') << endl;
+    h=I->h;
+    w=I->w;
+    for(int f=0;f<h;f++){
+        for(int c=0;c<w;c++){
+            cout << I->GetColor(c,f,'R') << "-" << I->GetColor(c,f,'G')<< "-" << I->GetColor(c,f,'B');
+            cout << "/";
+        }
+        cout << endl;
+    }
     delete I;
     return 0;
 }
